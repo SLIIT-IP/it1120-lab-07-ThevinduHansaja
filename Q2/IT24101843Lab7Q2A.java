3d648bff36b6ca1910763701e06fc758b87dcb10
Thevindu Hansaja <it24101843@my.sliit.lk>
2024-09-18 11:10:50 +0530

    IT24101843Lab7Q2A.java

---
+++ Q2/IT24101843Lab7Q2A.java
@@ -0,0 +1,15 @@
+public class IT24101843Lab7Q2A{
+public static void main(String[]args){
+int i;
+int j;
+for( i =1 ; i <= 4; i++)
+  {
+   for( j =1; j <= 4; j++)
+    {
+     System.out.print(" $ ");
+    }
+   	System.out.println(" ");
+   }
+ }
+}
+
